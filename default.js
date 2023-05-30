3f8e304f5019a9994782d090744e5c82d6271a63
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-05-30 11:38:19 +0900

    5월30일 3교시  자스 콘솔 typeof

--- default.js
+++ default.js
@@ -1,12 +1,21 @@
-document.write("Hello World");
-document.write("<h1>Welcom to JS Program</h1>");
-document.write("<h2>Welcome to Js Program</h2>")
+console.log(typeof 123);
+console.log(typeof  123.5);
+console.log(typeof "123");
+console.log(typeof  true);
+console.log(typeof false);
 
-console.log('Welcome to Js Program')
-console.info('Welcome to Js Program')
-console.warn('Welcome to Js Program')
-console.error('Welcome to Js Program')
 
-alert('Welcome JS Program');
-var a =prompt('Welcome to Js Program');
-console.log(a);
+// document.write("Hello World");
+// document.write("<h1>Welcom to JS Program</h1>");
+// document.write("<h2>Welcome to Js Program</h2>");
+//
+// console.log('Welcome to Js Program');
+// console.info('Welcome to Js Program');
+// console.warn('Welcome to Js Program');
+// console.error('Welcome to Js Program');
+//
+// alert('Welcome JS Program');
+// var a = prompt('Welcome to Js Program');
+// console.log(a);
+//  ++는 과거연산기록이있어야해서 못쓴다?
+// 동시실행으로 최적화가 많아졌는데 ++은 순차실행이라 속도에 문제가있다?

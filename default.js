a7c973814e9948f2cc9d6bc741a5df753253f16d
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-05-30 13:23:07 +0900

    5월30일 4교시  자스 문자열 연결

--- default.js
+++ default.js
@@ -1,9 +1,45 @@
-console.log(typeof 123);
-console.log(typeof  123.5);
-console.log(typeof "123");
-console.log(typeof  true);
-console.log(typeof false);
+var lName = '홍'
+var fName ='길동';
 
+var fullName =lName+fName;
+
+console.log(fullName);
+console.log("Today is"+ " a "+ "beaultful day");
+console.log("My name is "+ fullName);
+
+var num1 =20;
+var num2 = 30;
+var sum = num1+num2;
+console.log(num1+" + "+num2+" = "+sum);
+console.log(""+num1+num2);
+console.log(num1+num2);
+
+
+
+// var name = "이승훈";
+// var age =29;
+// var cgpa =3.92;
+// var lineBreak = "<br/>";
+//
+// document.write("이름: " + name + lineBreak);
+// document.write("나이: " + age + lineBreak);
+// document.write("학점: " + cgpa + lineBreak);
+//
+
+
+// console.log(typeof 123);
+// console.log(typeof  123.5);
+// console.log(typeof "123");
+// console.log(typeof  true);
+// console.log(typeof false);
+
+// var car;
+// console.log(typeof car);
+// var car =" ";
+// console.log(typeof car);
+// var person ={firstName: "John",lastNaMe:"Doe",age:50,eyeColor:"blue"};
+// console.log(typeof person);
+//
 
 // document.write("Hello World");
 // document.write("<h1>Welcom to JS Program</h1>");

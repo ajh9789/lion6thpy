059f2d7e29c049dc9b66fb9964d29816f2e80b6b
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-05-30 11:22:45 +0900

    5월30일 3교시  자스 콘솔

--- default.js
+++ default.js
@@ -1,1 +1,12 @@
-document.write("Hello World2");
+document.write("Hello World");
+document.write("<h1>Welcom to JS Program</h1>");
+document.write("<h2>Welcome to Js Program</h2>")
+
+console.log('Welcome to Js Program')
+console.info('Welcome to Js Program')
+console.warn('Welcome to Js Program')
+console.error('Welcome to Js Program')
+
+alert('Welcome JS Program');
+var a =prompt('Welcome to Js Program');
+console.log(a);

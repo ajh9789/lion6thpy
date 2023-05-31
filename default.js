d69abd09d95af66031f3b8d0fee9fbd5fb08907e
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-05-31 11:06:35 +0900

    5월31일 3교시  자스 if조건문  자음모음

--- default.js
+++ default.js
@@ -1,29 +1,36 @@
+var letter = prompt("Enter a letter : ");
 
-var num1 = parseInt(prompt('첫번째 숫자 입력 : '));
-var num2 = parseInt(prompt('두번쨰 숫자 입력 :'));
+letter = letter.toLowerCase();
 
-if(num1 < num2){
-    console.log("큰 수는 : "+ num2);
-}
-
-if(num1 > num2){
-    console.log('큰 수는 : '+ num1);
-}
-
-if(num1 == num2){
-    console.log('같은수');
-}
-
-if(num1 > num2){
-    console.log('큰 수 num1 : '+ num1);
-} else if (num1 < num2){
-    console.log("큰 수 num2 : " + num2);
-} else if (num1 == num2){
-    console.log("같은 수");
+if(letter == 'a' || letter == 'a'|| letter== 'i' || letter == 'o' || letter == 'u'){
+    console.log('Vowel');
+} else {
+    console.log('Consonant');
 }
 
 
-
+// var num1 = parseInt(prompt('첫번째 숫자 입력 : '));
+// var num2 = parseInt(prompt('두번쨰 숫자 입력 :'));
+//
+// if(num1 < num2){
+//     console.log("큰 수는 : "+ num2);
+// }
+//
+// if(num1 > num2){
+//     console.log('큰 수는 : '+ num1);
+// }
+//
+// if(num1 == num2){
+//     console.log('같은수');
+// }
+//
+// if(num1 > num2){
+//     console.log('큰 수 num1 : '+ num1);
+// } else if (num1 < num2){
+//     console.log("큰 수 num2 : " + num2);
+// } else if (num1 == num2){
+//     console.log("같은 수");
+// }
 
 // var num1 = 20;
 // var num2 = 10;

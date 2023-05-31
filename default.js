d539f168cfab04adba5db4b15f5b5abebf340c13
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-05-31 09:38:27 +0900

    5월31일 1교시  자스 비교연산자  >,<,!=,==등등

--- default.js
+++ default.js
@@ -1,12 +1,41 @@
+var num1 = 20;
+var num2 = 10;
+var num3 = "10";
+var num4 = 20;
+var num5 = 15;
 
-var base = parseFloat(prompt("밑변 입력"));
-var height = parseFloat(prompt("높이 입력"));
+console.log('일반 크기비교');
+console.log(num1 > num2, num1, ">", num2);
+console.log(num1 >= num2, num1, ">=", num2);
+console.log(num1 <= num2, num1, '<', num2);
+console.log(num1 <= num2, num1, '<=', num2);
 
-var area = number = base * height * 0.5;
+console.log('같은지 여부 확인')
+console.log(num1 == num4,num1, '==' ,num4 );
+console.log(num1 != num4, num1,'!=', num4);
 
-document.write("삼각형의 넓이 : " + area);
+//등호 3개는 타입까지 비교
+console.log('===');
+console.log(num1 === num3, num1,'===', num3);
+console.log(num2 === num3, num2, '===', num3);
+console.log(num2 == num3, num2, '==', num3);
 
+console.log('num1>num2 && num1>num5',num1>num2 && num1>num5);
+console.log('num1>num2 || num1>num5',num1>num2 || num1>num5);
 
+// var cels = parseFloat(prompt("섭씨 입력 : "));
+// var farn = cels * (9/5)+32;
+// document.write("화씨 : "+farn)
+
+//
+// var base = parseFloat(prompt("밑변 입력"));
+// var height = parseFloat(prompt("높이 입력"));
+//
+// var area = number = base * height * 0.5;
+//
+// document.write("삼각형의 넓이 : " + area);
+//
+//
 
 
 

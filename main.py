122fb0c5ba151c22c28d48f3688ef6f11d726654
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-05 13:11:44 +0900

    6월5일 파이썬 문법 산술 연산자

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
-# 변수 선언
-a=10
-b=3.14
-c="hello, world"
-d=[1,2,3]
-e=(4,5,6)
-f={7,8,9}
-g={"apple":1,"bananan":2,"orange":3}
+# 덧셈
+a = 4
+b = 2
+total = a + b
+print('total = a + b:', total)
 
-# 데이터 타입 출력
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
-print(type(g))
+#뺄셈
+a = 4
+b = 2
+total = a - b
+print('total = a - b: ', total)
+
+#곱셈
+total = a * b
+print('total = a * b: ', total)
+
+#나눗셈
+print('a / b: ', a / b)
+
+#나머지
+a = 5
+b = 2
+print('a % b: ', a % b)
+
+#거듭제곱
+print('a ** b: ', a ** b)
+
+# 몫 (양수)
+print('a // b : ', a // b)
+
+# 몫 (음수)
+a = -5
+print('a // b(음수) :', a // b)
+
+
+
+# # 변수 선언
+# a=10
+# b=3.14
+# c="hello, world"
+# d=[1,2,3]
+# e=(4,5,6)
+# f={7,8,9}
+# g={"apple":1,"bananan":2,"orange":3}
+#
+# # 데이터 타입 출력
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))

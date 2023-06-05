1adedb19b67fd97cf791b40e69829cdd6b4fc43c
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-05 14:04:08 +0900

    6월5일 파이썬 문법 4교시 논리,할당,비트,멤버in 연산자

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+# 멤버 in 연산자
 
+st1 = "Welcome to 멋쟁이 사자"
+print("to" not in st1)
 
-# # 멤버 in 연산자
-#
-# st1 = "Welcome to 멋쟁이 사자"
-# print("to" not in st1)
-#
-# st2 = "Welcome top 멋쟁이 사자"
-# print("to" not in st2)
-#
-# st3 = "Welcome to 멋쟁이 사자"
-# print("subs" not in st3)
+st2 = "Welcome top 멋쟁이 사자"
+print("to" not in st2)
+
+st3 = "Welcome to 멋쟁이 사자"
+print("subs" not in st3)
+
 
 
 # # 비트 연산자 실습

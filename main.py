6184c0886bfb9109241eb47aa551fb6ccc75cbd1
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-07 11:44:59 +0900

    6월7일 3교시 array pop() 실습

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
 from array import *
 
+
 stu_roll =array('i',[101,102,103,104,105])
-n=len(stu_roll)
-i=0
-while i<n:
+
+n = len(stu_roll)
+i = 0
+while i < n:
     print(stu_roll[i])
-    i +=1
+    i += 1
 
+print("배열 pop() 함수 실습")
 
-print("Array After Insert")
-stu_roll.insert(1, 106)
-stu_roll.insert(3, 107)
-n= len(stu_roll)
-i=0
-while i<n:
+element = stu_roll.pop(0)
+print('element:', element)
+n = len(stu_roll)
+i = 0
+while i < n:
     print(stu_roll[i])
-    i+=1
+    i += 1
 
-print("배열 요소 삭제")
 
-stu_roll.remove(107)
-n=len(stu_roll)
-i=0
-while i<n:
-    print(stu_roll[i])
-    i +=1
+# n=len(stu_roll)
+# i=0
+# while i<n:
+#     print(stu_roll[i])
+#     i +=1
+#
+#
+# print("Array After Insert")
+# stu_roll.insert(1, 106)
+# stu_roll.insert(3, 107)
+# n= len(stu_roll)
+# i=0
+# while i<n:
+#     print(stu_roll[i])
+#     i+=1
+#
+# print("배열 요소 삭제")
+#
+# stu_roll.remove(107)
+# n=len(stu_roll)
+# i=0
+# while i<n:
+#     print(stu_roll[i])
+#     i +=1
 
 # # 틀만 만들어두고 TODO를 사용하면 협업할때 좋음
 # a = 5

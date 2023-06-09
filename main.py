57e0dc52e3b72915afa89fa5cabf37bb303f5272
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-09 13:29:29 +0900

    6월9일 4교시 파일 읽기

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from datetime import timedelta
-from datetime import date
-from datetime import datetime
+file_object = open('example.txt', 'r')
 
-td = timedelta(days=10)
-print(td)
+content = file_object.read()
+print(content)
 
-d1 = date(year=2023, month=5, day=5)
-d2 = date(year=2023, month=6, day=9)
-
-# 날짜의 연산자 오버로딩으로 비교할 수 있습니다.
-print(d1 == d2)
-print(d1 < d2)
-print(d1 > d2)
-
-dt = datetime.today()
-
-formatted_datetime = dt.strftime('%B, %d, %Y')
-print(formatted_datetime)
-
-
+file_object.close()
+# from datetime import timedelta
+# from datetime import date
+# from datetime import datetime
+#
+# td = timedelta(days=10)
+# print(td)
+#
+# d1 = date(year=2023, month=5, day=5)
+# d2 = date(year=2023, month=6, day=9)
+#
+# # 날짜의 연산자 오버로딩으로 비교할 수 있습니다.
+# print(d1 == d2)
+# print(d1 < d2)
+# print(d1 > d2)
+#
+# dt = datetime.today()
+#
+# formatted_datetime = dt.strftime('%B, %d, %Y')
+# print(formatted_datetime)
 
 # class ParentClass:
 #     def __init__(self):

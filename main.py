9874eaa90fdc4bd52ef36a7e274b3a1611c7d449
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-09 14:26:41 +0900

    6월9일 5교시 폴더,중첩폴더 만들기

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 import os
 
-filename = 'example.txt'
+curret_directory = os.getcwd()
+print(curret_directory)
 
-print("파일이 존재하는지 확인하기")
-if os.path.isfile(filename):
-    print(f"{filename}이 존재합니다.")
-else:
-    print(f"{filename}이 없습니다.")
+#os.mkdir('new_directory')
+#os.makedirs('parent_directory/child_directory/grandchild_directory1/2/3/4/5')
 
-file_object = open('list_example.txt', 'w')
 
-content_list=["Python", "Java", "C++", "Javascript"]
-
-for item in content_list:
-    file_object.write(item + '\n')
-
-file_object.close()
 
 # file_object =open('new_example.txt','w')
 #

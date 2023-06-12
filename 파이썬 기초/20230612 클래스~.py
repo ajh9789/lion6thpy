48e9f3b1060f7ff9652148ce603f087372554d42
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-12 10:40:18 +0900

    6월12일 2교시 파이썬 다중상속

--- 파이썬 기초/20230612 클래스~.py
+++ 파이썬 기초/20230612 클래스~.py
@@ -1,19 +1,37 @@
-class Vehicle:
-    def __init__(self, make, model, year):
-        self.make =make
-        self.model=model
-        self.year=year
+class Engine:
+    def start(self):
+        return "Engine started"
+    def stop(self):
+        return "Engine stopped"
 
-    def start_engine(self):
-        return "The engine is running!"
+class Wheels:
+    def rotate(self):
+        return "Whells are rotating"
+#다중 상속
+class Car(Engine, Wheels):
+    pass
+#인스턴스 생성
+my_car =Car()
+# 부모 클래스의 메소드 사용
+print(my_car.start())
+print(my_car.rotate())
 
-class Car(Vehicle):
-    def start_engine(self):
-        return  super().start_engine() + " It's a car engine."
-
-my_car = Car("Toyotam", "Corolla", 2020)
-
-print(my_car.start_engine())
+# class Vehicle:
+#     def __init__(self, make, model, year):
+#         self.make =make
+#         self.model=model
+#         self.year=year
+#
+#     def start_engine(self):
+#         return "The engine is running!"
+#
+# class Car(Vehicle):
+#     def start_engine(self):
+#         return  super().start_engine() + " It's a car engine."
+#
+# my_car = Car("Toyotam", "Corolla", 2020)
+#
+# print(my_car.start_engine())
 
 # class Car:
 #     # 클래스 속성

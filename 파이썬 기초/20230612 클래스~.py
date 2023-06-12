2c9e3787be210614a7914e55494720efa8fc6eda
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-12 10:36:16 +0900

    6월12일 2교시 파이썬 상속

--- 파이썬 기초/20230612 클래스~.py
+++ 파이썬 기초/20230612 클래스~.py
@@ -1,20 +1,41 @@
-class Car:
-    wheels = 4
-
-    def __init__(self, make, model, color):
+class Vehicle:
+    def __init__(self, make, model, year):
         self.make =make
         self.model=model
-        self.color=color
-    def drive(self):
-        return "The car is moving!"
-    def stop(self):
-        return "The car has stopped."
+        self.year=year
 
-my_car =Car("Kia","Morning", "Blue")
+    def start_engine(self):
+        return "The engine is running!"
 
-#속성 사용하기
-print(my_car.make)
+class Car(Vehicle):
+    def start_engine(self):
+        return  super().start_engine() + " It's a car engine."
 
-#메소드 호출
-print(my_car.drive())
-print(my_car.stop())
+my_car = Car("Toyotam", "Corolla", 2020)
+
+print(my_car.start_engine())
+
+# class Car:
+#     # 클래스 속성
+#     wheels = 4
+#
+#     def __init__(self, make, model, color):
+#         self.make = make
+#         self.model = model
+#         self.color = color
+#     # 매소드
+#     def drive(self):
+#         return "The car is moving!"
+#
+#     def stop(self):
+#         return f"The car has stopped."
+#
+#
+# my_car = Car("Kia", "Morning", "Blue")
+#
+# # 속성 사용
+# print(my_car.make)
+#
+# # 메소드 호출
+# print(my_car.drive())
+# print(my_car.stop())

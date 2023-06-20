2d38918b48b325e985559ce8893819f587a04cbd
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-20 15:06:20 +0900

    6월20일 polls 앱 생성, 초기설정

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-p+#kcdzrzu8yx&jgmt%#%i5xd_76+4ppgtz$gza2%(856eg4xw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
